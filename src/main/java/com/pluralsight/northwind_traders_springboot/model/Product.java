0a89beebb31f7cdc1fb240100025eccff221d2a6
lovitesfay <lovitesfay@gmail.com>
2026-06-15 15:15:06 -0500

    exercise 1

---
+++ src/main/java/com/pluralsight/northwind_traders_springboot/model/Product.java
@@ -0,0 +1,10 @@
+package com.pluralsight.northwind_traders_springboot.model;
+
+public class Product {
+   private int productId;
+   private String productName;
+   private String Category;
+   private double price;
+
+
+}
